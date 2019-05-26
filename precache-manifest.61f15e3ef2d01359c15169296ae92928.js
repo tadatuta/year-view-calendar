588590263e92b62f9c247d82ba3f5b8a73a81218
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-26 13:43:08 +0000

    Deploy tadatuta/year-view-calendar to github.com/tadatuta/year-view-calendar.git:gh-pages

---
+++ precache-manifest.61f15e3ef2d01359c15169296ae92928.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34cb77ef18526445e8d27de935ef2dd4",
+    "url": "/year-view-calendar/index.html"
+  },
+  {
+    "revision": "372f6c69851f3fcaf1ed",
+    "url": "/year-view-calendar/static/css/main.88c060e0.chunk.css"
+  },
+  {
+    "revision": "de0193c2731a6a5c02f9",
+    "url": "/year-view-calendar/static/js/2.53d78030.chunk.js"
+  },
+  {
+    "revision": "372f6c69851f3fcaf1ed",
+    "url": "/year-view-calendar/static/js/main.5f765395.chunk.js"
+  },
+  {
+    "revision": "2af3258796dc6695aaab",
+    "url": "/year-view-calendar/static/js/runtime~main.b55ea234.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/year-view-calendar/precache-manifest.31a4af6e6fab6b4e70ccce893f1f5a74.js"
+  "/year-view-calendar/precache-manifest.61f15e3ef2d01359c15169296ae92928.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/year-view-calendar/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/year-view-calendar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
