f584f99da4cf94fceade779323e9dd3ad36f0b83
Vladimir Grinenko <i@tadatuta.com>
2019-12-13 13:22:14 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import data from './events.js';
 class App extends Component {
   render() {
     return (
-      <Calendar data={data} year="2020" />
+      <Calendar data={data} year={2020} />
     );
   }
 }
