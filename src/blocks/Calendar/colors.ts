2af5a0d7fcc2576ce2981281607bda76b86606f1
Vladimir Grinenko <i@tadatuta.com>
2020-01-08 01:54:30 +0300

    add colors

--- src/blocks/Calendar/colors.ts
+++ src/blocks/Calendar/colors.ts
@@ -1,5 +1,5 @@
 export const colors = [
-    '#ccc', '#14A0E7', 'red', 'rebeccapurple',
+    '#eee', '#ccc', '#14A0E7', 'red', 'rebeccapurple',
     '#A2EEFE', '#649B5E', '#dfd114', '#E59942',
-    '#49B851', '#79B473'
+    '#49B851', '#b47394', '#fcea48', '#004080',
 ];
