425a1cefca9c52c32963a5130326595acd1d2373
Travis CI User <travis@example.org>
2020-01-07 17:58:42 +0000

    Updates

---
+++ precache-manifest.ef4c1a8da28ebf891bb233aa02176a76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcf8962416ed114277ea30e8a033db7",
+    "url": "/year-view-calendar/index.html"
+  },
+  {
+    "revision": "e55af72b5e9c9d1c4662",
+    "url": "/year-view-calendar/static/css/main.88c060e0.chunk.css"
+  },
+  {
+    "revision": "663e90e8722e2c2db6b7",
+    "url": "/year-view-calendar/static/js/2.887885b8.chunk.js"
+  },
+  {
+    "revision": "e55af72b5e9c9d1c4662",
+    "url": "/year-view-calendar/static/js/main.a17a2164.chunk.js"
+  },
+  {
+    "revision": "2af3258796dc6695aaab",
+    "url": "/year-view-calendar/static/js/runtime~main.b55ea234.js"
+  }
+]);
