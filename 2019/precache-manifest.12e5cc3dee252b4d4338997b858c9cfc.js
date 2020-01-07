e89b78ca6033da985e919f24ce9fb6318d4383d0
Vladimir Grinenko <i@tadatuta.com>
2020-01-07 21:14:11 +0300

    archive 2019

---
+++ 2019/precache-manifest.12e5cc3dee252b4d4338997b858c9cfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e3acd80041ca309ea44b4b7ba75abf",
+    "url": "/year-view-calendar/2019/index.html"
+  },
+  {
+    "revision": "81234ccfdd51a9cc0e4e",
+    "url": "/year-view-calendar/2019/static/css/main.88c060e0.chunk.css"
+  },
+  {
+    "revision": "663e90e8722e2c2db6b7",
+    "url": "/year-view-calendar/2019/static/js/2.887885b8.chunk.js"
+  },
+  {
+    "revision": "81234ccfdd51a9cc0e4e",
+    "url": "/year-view-calendar/2019/static/js/main.4f80868b.chunk.js"
+  },
+  {
+    "revision": "88874a976d66d709173e",
+    "url": "/year-view-calendar/2019/static/js/runtime~main.c10dc6f3.js"
+  }
+]);
