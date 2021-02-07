6e413dd71f43fcc4490ae13fd8d4499d0eab13bc
Vladimir Grinenko <i@tadatuta.com>
2021-02-07 20:32:49 +0300

    Update dependencies

--- src/blocks/Button/Button.stories.tsx
+++ src/blocks/Button/Button.stories.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { Button, ButtonProps } from './Button';
 
-import { Button } from './Button';
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
 
-storiesOf('Button', module)
-    .add('with text', () => <Button onClick={action('click')}>Тыкни меня!</Button>);
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
--- src/blocks/Button/Button.tsx
+++ src/blocks/Button/Button.tsx
@@ -1,17 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './button.css';
 
-interface IButton {
-    /**
-     * Позови меня клик
-     */
-    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
 }
 
 /**
- * Кнопка. Чтобы тыкать
+ * Primary UI component for user interaction
  */
-export class Button extends Component<IButton> {
-    render() {
-        return <button onClick={this.props.onClick}>{this.props.children}</button>;
-    }
-}
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
